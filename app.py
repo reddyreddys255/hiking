4429fa01345a96fadc48cc4969bae151b0247702
Perry Johnson <37843586+perryrjohnson@users.noreply.github.com>
2019-06-23 23:09:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -39,17 +39,17 @@ def top_five(recs):
 
 @app.route('/')
 def index():
-	return render_template('../web_app/templates/index.html')
+	return render_template('web_app/templates/index.html')
 
 @app.route('/username', methods=['GET', 'POST'])
 def username():
 	names = user_ids.keys()
-	return render_template('../web_app/templates/username.html', names=names)
+	return render_template('web_app/templates/username.html', names=names)
 
 @app.route('/choose-hike', methods=['GET', 'POST'])
 def enter_hike():
 	hikes = list_hikes(sf_hikes)
-	return render_template('../web_app/templates/choose-hike.html', hikes=hikes)
+	return render_template('web_app/templates/choose-hike.html', hikes=hikes)
 
 @app.route('/personal-recs', methods=['GET', 'POST'])
 def enter_username():
@@ -57,20 +57,20 @@ def enter_username():
 	try:
 		user_id = user_ids[username]
 	except:
-		return render_template('../web_app/templates/error.html')	
+		return render_template('web_app/templates/error.html')	
 	if username == '':
-		return render_template('../web_app/templates/error.html')
+		return render_template('web_app/templates/error.html')
 	else:
 		recs = rf_model.recommend(users = [user_id], k=5)
 		print(recs)
 		hike_data = get_hike_info(recs)
-		return render_template('../web_app/templates/personal-recs.html', hike_data=hike_data)
+		return render_template('web_app/templates/personal-recs.html', hike_data=hike_data)
 
 @app.route('/make-recommendations', methods=['POST', 'GET'])
 def get_recommendations():
 	hike = request.form.get('hike-name')
 	if hike == '':
-		return render_template('../web_app/templates/error.html')
+		return render_template('web_app/templates/error.html')
 	else:
 		recs = content_model.recommend_from_interactions(observed_items=[hike], k=10)
 		hike_data = []
@@ -80,7 +80,7 @@ def get_recommendations():
 			info['score'] = "{:.1%}".format(rec['score'])
 			hike_data.append(info)
 		your_hike = get_info(hike)
-		return render_template('../web_app/templates/make-recommendations.html', your_hike=your_hike, hike_data=hike_data)
+		return render_template('web_app/templates/make-recommendations.html', your_hike=your_hike, hike_data=hike_data)
 
 @app.route('/most-reviewed', methods=['POST', 'GET'])
 def get_popular():
@@ -90,7 +90,7 @@ def get_popular():
         hike = h_id['hike_id']
         recs.append(hike)
     best_hikes = top_five(recs)
-    return render_template('../web_app/templates/most-reviewed.html', best_hikes=best_hikes)
+    return render_template('web_app/templates/most-reviewed.html', best_hikes=best_hikes)
 
 @app.route('/highest-rated', methods=['POST', 'GET'])
 def get_highest():
@@ -100,7 +100,7 @@ def get_highest():
         hike = h_id['hike_id']
         recs.append(hike)   
     best_hikes = top_five(recs)
-    return render_template('../web_app/templates/highest-rated.html', best_hikes=best_hikes)
+    return render_template('web_app/templates/highest-rated.html', best_hikes=best_hikes)
 
 @app.route('/power-rated', methods=['POST', 'GET'])
 def get_power():
@@ -110,24 +110,24 @@ def get_power():
 		hike = h_id['hike_id']
 		recs.append(hike)
 	best_hikes = top_five(recs)	
-	return render_template('../web_app/templates/power-ratings.html', best_hikes=best_hikes)
+	return render_template('web_app/templates/power-ratings.html', best_hikes=best_hikes)
 
 if __name__ == '__main__':
 	reload(sys)
 	sys.setdefaultencoding('utf8')
-	sf_hikes = tc.SFrame('../Data/all_hikes_with_hike_name.csv')
+	sf_hikes = tc.SFrame('Data/all_hikes_with_hike_name.csv')
 	hikes = list(sf_hikes['hike_name'])
-	sf_ratings = tc.SFrame('../Data/all_ratings_matrix.csv')
-	hike_side_data = tc.SFrame('../Data/all_hikes_with_hike_id.csv')
-	with open('../Data/all_hike_ids.pkl') as f:
+	sf_ratings = tc.SFrame('Data/all_ratings_matrix.csv')
+	hike_side_data = tc.SFrame('Data/all_hikes_with_hike_id.csv')
+	with open('Data/all_hike_ids.pkl') as f:
 		hike_ids = pickle.load(f)
 
 	with open('../Data/all_user_ids.pkl') as f:
 		user_ids = pickle.load(f)
 
-	content_model = tc.load_model('../web_app/hike_content_recommender')
-	popular_count_model = tc.load_model('../web_app/hike_popularity_count_recommender')
-	popular_stars_model = tc.load_model('../web_app/hike_popularity_stars_recommender')
-	rf_model = tc.load_model('../web_app/rank_factorization_recommender')
+	content_model = tc.load_model('web_app/hike_content_recommender')
+	popular_count_model = tc.load_model('web_app/hike_popularity_count_recommender')
+	popular_stars_model = tc.load_model('web_app/hike_popularity_stars_recommender')
+	rf_model = tc.load_model('web_app/rank_factorization_recommender')
 
 	app.run()
